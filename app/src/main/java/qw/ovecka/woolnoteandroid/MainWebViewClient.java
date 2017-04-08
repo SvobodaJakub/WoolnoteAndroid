ffbb13f9c9e476e8b1ec04c22088e910d7a72920
Jakub Svoboda <SvobodaJakub@users.noreply.github.com>
2017-04-08 23:16:01 +0200

    The WebView now retries loading the webpage every second, so that the interface appears as soon as the python-based woolnote web server starts after app launch.

--- app/src/main/java/qw/ovecka/woolnoteandroid/MainWebViewClient.java
+++ app/src/main/java/qw/ovecka/woolnoteandroid/MainWebViewClient.java
@@ -23,4 +23,29 @@ public class MainWebViewClient extends WebViewClient {
         view.getContext().startActivity(intent);
         return true;
     }
+
+    @Override
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+        if (view != null) {
+
+            String htmlData = "<html>" +
+                    "<head>" +
+                    "<meta charset=utf-8>\n" +
+                    "<meta name=\"viewport\" content=\"width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, user-scalable=0\"/>" +
+                    "<meta http-equiv=\"refresh\" content=\"1;url=" + failingUrl + "\" />" +
+                    "</head>" +
+                    "<body>" +
+                    "<div><p>The page " + failingUrl + " didn't load, retrying...</p><br>" +
+                    "<p>Error description: " + description + "\n</p><br>" +
+                    "<p>This might be caused by the Woolnote server taking a few seconds to start.</p>" +
+                    "</div>" +
+                    "</body>";
+
+            view.loadUrl("about:blank");
+            view.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null);
+            view.invalidate();
+
+        }
+        super.onReceivedError(view, errorCode, description, failingUrl);
+    }
 }
